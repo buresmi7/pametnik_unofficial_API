1615293b104b6bc3c29b3d2205c2fafc94b51162
Michal <michal.ezop@gmail.com>
2017-07-04 14:59:46 +0200

    Upravy pro lambda proxy

--- app/index.js
+++ app/index.js
@@ -3,7 +3,39 @@
 
 const httpHandler = require('./httpHandler');
 
+const done = function(err, res, callback) {
+  var bodyStr;
+  if (err) {
+    bodyStr = JSON.stringify({
+      result: "error",
+      errorDetail: err
+    });
+  } else {
+    bodyStr = JSON.stringify({
+      result: "success",
+      response: res
+    });
+  }
+
+  callback(null, {
+    statusCode: err ? '400' : '200',
+    body: bodyStr,
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+};
+
 exports.userMedalsList = (event, context, callback) => {
     const userMedalsList = require('./userMedalsList');
-    userMedalsList(httpHandler, event.token, callback);
+
+    if (!event.queryStringParameters.token) {
+        return done('Token parameter missing.', null, callback);
+    }
+
+    console.log("Token: " + event.queryStringParameters.token);
+
+    userMedalsList(httpHandler, event.queryStringParameters.token, function(err, res) {
+        done(err, res, callback);
+    });
 };
